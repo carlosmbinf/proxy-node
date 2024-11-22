6131e324e23cd6ff51e58b7eac63856a251ca861
Carlos Medina <carlosmbinf@gmail.com>
2024-11-22 11:44:44 -0300

    agregos de https greenlock

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
-// const proxy = require('@ucipass/proxy')
-// const proxyPort = 80
-// proxy(proxyPort)
-// .then(()=>{
-//   // Use it for a while....
-//   console.log("Puerto iniciado por: "+ proxyPort );
-// })
-// .then((server) => {
-//   // console.log(server);
-//   // server.stop() 
-// })
+const Greenlock = require("greenlock-express");
+const httpProxy = require("http-proxy");
 
-var fs = require("fs");
-var appRoot = require("app-root-path");
-var httpProxy = require('http-proxy');
+// Crear el servidor proxy reverso
+const proxy = httpProxy.createProxyServer({
+    target: "http://127.0.0.1:6000", // Servidor Meteor
+    changeOrigin: true,
+    ws: true,
+    xfwd: true,
+});
 
-var PATH_TO_KEY = appRoot.path + '/conf/28459803_srv5119-206152.vps.etecsa.cu.key';
- var PATH_TO_CERT = appRoot.path + '/conf/28459803_srv5119-206152.vps.etecsa.cu.cert';
+// Manejar eventos del proxy
+proxy.on("error", (err, req, res) => {
+    console.error("Error en el proxy:", err.message);
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("Error en el servidor proxy.");
+});
 
-var options = {
- ssl: {
- key: fs.readFileSync(PATH_TO_KEY, 'utf8'),
- cert: fs.readFileSync(PATH_TO_CERT, 'utf8')
- },
- target : 'http://localhost:6000',
- ws: true,
- xfwd: true,
-};
-var server = httpProxy.createProxyServer(options).listen(443);
-console.log('httpProxy running with target at ' + options.target);
+// Configuración de Greenlock
+Greenlock.init({
+    packageRoot: __dirname,
+    configDir: "./greenlock.d",
+    maintainerEmail: "carlosmbinf@gmail.com", // Reemplaza con tu correo
+    cluster: false,
+    sites: [
+        {
+            subject: "vidkar.ddns.net", // Cambia esto por tu dominio
+            altnames: ["vidkar.ddns.net"], // Alias adicionales
+        },
+    ],
+})
+    .serve((req, res) => {
+        // Redirigir todas las solicitudes HTTPS al proxy reverso
+        proxy.web(req, res);
+    });
